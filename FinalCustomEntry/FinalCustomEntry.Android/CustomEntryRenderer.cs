51e8aa22369e106d7c4a718e316a9b5df31741f5
El_makso <El_makso@mail.ru>
2020-06-08 07:29:22 +0300

    q

---
+++ FinalCustomEntry/FinalCustomEntry.Android/CustomEntryRenderer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using FinalCustomEntry;
+using FinalCustomEntry.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace FinalCustomEntry.Droid
+{
+    public class CustomEntryRenderer: EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                var view = (CustomEntry)Element;
+
+                var _gradientBackground = new GradientDrawable();
+                _gradientBackground.SetShape(ShapeType.Rectangle);
+                _gradientBackground.SetColor(view.BackgroundColor.ToAndroid());
+                _gradientBackground.SetStroke(view.BorderWidth, view.BorderColor.ToAndroid());
+                _gradientBackground.SetCornerRadius(DpToPixels(this.Context, Convert.ToSingle(view.CornerRadius)));  
+                Control.SetBackground(_gradientBackground);
+
+                this.Control.SetRawInputType(InputTypes.TextFlagNoSuggestions);
+
+                this.Control.SetPadding(
+                    (int)DpToPixels(this.Context, view.LeftPadding),
+                     (int)DpToPixels(this.Context, view.TopPadding),
+                    (int)DpToPixels(this.Context, view.RightPadding),
+                    (int)DpToPixels(this.Context, view.BottomPadding)
+                    );
+
+                if (view.IsNumeric)
+                {
+                    Control.InputType = Android.Text.InputTypes.ClassNumber | Android.Text.InputTypes.NumberFlagSigned | Android.Text.InputTypes.NumberFlagDecimal;
+                }
+                if (view.IsPasswordK)
+                {
+                    Control.InputType = Android.Text.InputTypes.TextVariationWebEmailAddress | Android.Text.InputTypes.TextVariationEmailAddress;
+                }
+                if (view.IsMail)
+                {
+                    Control.InputType = Android.Text.InputTypes.TextVariationWebPassword; //Android.Text.InputTypes.TextVariationPassword | 
+                }
+            }
+        }
+        public static float DpToPixels(Context context, float valueInDp)
+        {
+            DisplayMetrics metrics = context.Resources.DisplayMetrics;
+            return TypedValue.ApplyDimension(ComplexUnitType.Dip, valueInDp, metrics);
+        }
+    }
+}
---
+++ FinalCustomEntry/FinalCustomEntry/CustomEntry.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FinalCustomEntry
+{
+    public class CustomEntry : Entry
+    {
+
+        public static readonly BindableProperty IsNumericProperty =
+        BindableProperty.Create(
+         nameof(IsNumeric),
+         typeof(bool),
+         typeof(CustomEntry),
+         false
+         );
+        public bool IsNumeric
+        {
+            get => (bool)GetValue(IsNumericProperty);
+            set => SetValue(IsNumericProperty, value);
+        }
+
+
+        public static readonly BindableProperty IsPasswordKProperty =
+        BindableProperty.Create(
+         nameof(IsPasswordK),
+         typeof(bool),
+         typeof(CustomEntry),
+         false
+         );
+        public bool IsPasswordK
+        {
+            get => (bool)GetValue(IsPasswordKProperty);
+            set => SetValue(IsPasswordKProperty, value);
+        }
+
+
+        public static readonly BindableProperty IsMailProperty =
+       BindableProperty.Create(
+        nameof(IsMail),
+        typeof(bool),
+        typeof(CustomEntry),
+        false
+        );
+        public bool IsMail
+        {
+            get => (bool)GetValue(IsMailProperty);
+            set => SetValue(IsMailProperty, value);
+        }
+
+
+        public static readonly BindableProperty BorderColorProperty =
+        BindableProperty.Create(
+         nameof(BorderColor),
+         typeof(Color),
+         typeof(CustomEntry),
+         Color.Gray
+         );
+        public Color BorderColor
+        {
+            get => (Color)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
+        }
+
+
+        public static readonly BindableProperty BorderWidthProperty =
+        BindableProperty.Create(
+            nameof(BorderWidth),
+            typeof(int),
+            typeof(CustomEntry),
+            1
+            );
+        public int BorderWidth
+        {
+            get => (int)GetValue(BorderWidthProperty);
+            set => SetValue(BorderWidthProperty, value);
+        }
+
+        public static readonly BindableProperty CornerRadiusProperty =
+        BindableProperty.Create(
+            nameof(CornerRadius),
+            typeof(double),
+            typeof(CustomEntry),
+            5d
+            );
+        public double CornerRadius
+        {
+            get => (double)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public static readonly BindableProperty TopPaddingProperty =
+      BindableProperty.Create(
+          nameof(TopPadding),
+          typeof(int),
+          typeof(CustomEntry),
+          0
+          );
+        public int TopPadding
+        {
+            get => (int)GetValue(TopPaddingProperty);
+            set => SetValue(TopPaddingProperty, value);
+        }
+
+        public static readonly BindableProperty LeftPaddingProperty =
+      BindableProperty.Create(
+          nameof(LeftPadding),
+          typeof(int),
+          typeof(CustomEntry),
+          0
+          );
+        public int LeftPadding
+        {
+            get => (int)GetValue(LeftPaddingProperty);
+            set => SetValue(LeftPaddingProperty, value);
+        }
+
+        public static readonly BindableProperty RightPaddingProperty =
+     BindableProperty.Create(
+         nameof(RightPadding),
+         typeof(int),
+         typeof(CustomEntry),
+         0
+         );
+        public int RightPadding
+        {
+            get => (int)GetValue(RightPaddingProperty);
+            set => SetValue(RightPaddingProperty, value);
+        }
+
+
+        public static readonly BindableProperty BottomPaddingProperty =
+    BindableProperty.Create(
+        nameof(BottomPadding),
+        typeof(int),
+        typeof(CustomEntry),
+        0
+        );
+        public int BottomPadding
+        {
+            get => (int)GetValue(BottomPaddingProperty);
+            set => SetValue(BottomPaddingProperty, value);
+        }
+    }
+}
